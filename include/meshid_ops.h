cccfc0aaed56ad285da9eba93d308f7a8bbaf258
ryuzot <ryuzot.1112@gmail.com>
2025-01-02 18:44:17 +0900

    Add cmph-git as a submodule

---
+++ include/meshid_ops.h
@@ -0,0 +1,8 @@
+//
+// Created by ryuzot on 25/01/02.
+//
+
+#ifndef MESHID_OPS_H
+#define MESHID_OPS_H
+
+#endif //MESHID_OPS_H
---
+++ src/meshid_ops.c
@@ -0,0 +1,5 @@
+//
+// Created by ryuzot on 25/01/02.
+//
+
+#include "meshid_ops.h"
